9c78a669fc481980509b94562cbbc83ab6f4f083
Gomuro <yra3908@gmail.com>
2022-06-16 22:09:54 +0300

    add some functoional

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 
-const SideBar = () => {
+const SideBar = ({
+  notes,
+  addNote,
+  activeNote,
+  deleteNote,
+  getActiveNote,
+  setActiveNoteId,
+  editNote,
+}) => {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>Notes</h1>
-        <button>Add</button>
+        <button onClick={addNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
-        <div className="app-sidebar-note">
-          <div className="sidebar-note-title">
-            <strong>Title</strong>
-            <button>Delete</button>
+        {notes.map(({ title, body, id }) => (
+          <div
+            className={`app-sidebar-note ${id === activeNote && "active"}`}
+            onClick={() => setActiveNoteId(id)}
+          >
+            <div className="sidebar-note-title">
+              <strong>{title}</strong>
+              <button onClick={() => deleteNote(id)}>Delete</button>
+            </div>
+            <p>{body.length > 50 ? `${body.substring(0, 50)}...` : body}</p>
           </div>
-          <p>Not preview</p>
-        </div>
+        ))}
       </div>
     </div>
   );
